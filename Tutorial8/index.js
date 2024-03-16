241185674443f2b7a4aab74087f6826b599545b8
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-03-16 12:12:28 +0600

    Add files via upload

---
+++ Tutorial8/index.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const mongoose = require('mongoose');
+const app=express();
+
+const productSchema=new mongoose.Schema({
+    title:String,
+    price:Number, 
+    description:String,
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+});
+const Product=mongoose.model("Products",productSchema);
+
+const connectDB=async()=>
+{
+    try{
+        await mongoose.connect('mongodb://localhost:27017/testProdeuct');
+        console.log("DB is connected");
+    }
+    catch{
+        console.log("DB is not Connected ",error);
+        process.exit(1);
+    }
+};
+
+
+const port=3002;
+console.log("Khalid")
+app.get("/",(req,res)=>{
+    res.send("Welcome to Home Page");
+})
+app.listen(port,async()=>
+{
+    console.log(`Server is running at http://localhost:${port}`);
+    await connectDB();
+});
